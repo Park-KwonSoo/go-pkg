cb5fa6cdcbe7da27ce2dc3292afd59f79308187e
Park-Kwonsoo <alyxkwonsoo@gmail.com>
2022-04-21 14:16:27 +0900

    [박권수] feat. 한글 초성 가져오는 패키지 생성

---
+++ test/hangul/hangul_test.go
@@ -0,0 +1,20 @@
+package hangul_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Park-KwonSoo/go-pkg/pkg/hangul"
+)
+
+func TestIsChosung(t *testing.T) {
+	rslt := hangul.GetCHO("안녕하세요")
+	if rslt != "ㅇㄴㅎㅅㅇ" {
+		t.Errorf(fmt.Sprintf("It's not string's chosung: %s", rslt))
+	}
+
+	rslt = hangul.GetCHO("사과")
+	if rslt != "ㅅㄱ" {
+		t.Errorf(fmt.Sprintf("It's not string's chosung: %s", rslt))
+	}
+}
